091f61883876303be48a3a932fd8157c498d2b4b
Joana Rozendo <joanarozendo@gmail.com>
2019-11-13 18:13:32 +0000

    done minus the bonus iteration

--- starter_code/app.js
+++ starter_code/app.js
@@ -16,6 +16,41 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beersObject => {
+    res.render('beers', {
+      beers: beersObject
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randomBeer => {
+    res.render('random-beer', {
+      beer: randomBeer
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+/* app.get('/random-beer/:random-beer_id', (req, res, next) => {
+  punkAPI.getBeer()
+  .then(randomBeer => {
+    res.send(req.params.random-beer_id);
+    res.render('random-beer', {
+      beer: randomBeer
+    });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}); */
 
 app.listen(3000);
